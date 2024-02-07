383d3492c167e34c48e91ecee4603f0b86ffceab
Kristifor <km@aizen.io>
2024-02-07 21:10:33 +0200

    Adding an Authorization interface to handle locking/unlocking.

---
+++ lib/interfaces/iauthorization_service.dart
@@ -0,0 +1,9 @@
+import 'package:domain/models/enums.dart';
+
+abstract class IAuthorizationService {
+  Future<bool> isDeviceLocked();
+  Future<DeviceLockType> getDeviceLockType();
+  Future<bool> setDeviceLockType(DeviceLockType type, {String? value});
+  Future<bool> unlockPin(String value);
+  Future<bool> unlockTotp(String code);
+}
