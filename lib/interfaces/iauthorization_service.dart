02bdf810de5a020f537b9ccb57bad08f2a563563
Kristifor <km@aizen.io>
2024-02-09 17:23:26 +0200

    Adding the option to enable biometric login

--- lib/interfaces/iauthorization_service.dart
+++ lib/interfaces/iauthorization_service.dart
@@ -6,4 +6,5 @@ abstract class IAuthorizationService {
   Future<bool> setDeviceLockType(DeviceLockType type, {String? value});
   Future<bool> unlockPin(String value);
   Future<bool> unlockTotp(String code);
+  Future<bool> enableBiometric();
 }
