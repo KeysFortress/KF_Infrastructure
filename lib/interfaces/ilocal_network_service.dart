ef92e401ad18d994a46ab725995d9f39d1cc9f9c
Kristifor <km@aizen.io>
2024-02-14 17:10:42 +0200

    Adding a new method to get communication credentials

--- lib/interfaces/ilocal_network_service.dart
+++ lib/interfaces/ilocal_network_service.dart
@@ -1,6 +1,8 @@
+import 'package:cryptography/cryptography.dart';
 import 'package:domain/models/device.dart';
 
 abstract class ILocalNetworkService {
   Future<List<Device?>> scan();
   Future<Device> getNetworkData();
+  Future<SimpleKeyPair> getCredentails();
 }
