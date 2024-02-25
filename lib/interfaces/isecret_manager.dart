19c1a58442e2ef6075d481187d6b448c87f13e40
Kristifor <km@aizen.io>
2024-02-25 14:06:51 +0200

    Extending the generate secret to support optional parameters.

--- lib/interfaces/isecret_manager.dart
+++ lib/interfaces/isecret_manager.dart
@@ -1,7 +1,14 @@
 import 'package:domain/models/stored_secret.dart';
 
 abstract class ISecretManager {
-  String generateSecret({int length = 12});
+  String generateSecret({
+    int length = 12,
+    bool isSecial = true,
+    bool isUpper = true,
+    bool isLower = true,
+    bool isDigit = true,
+    bool isUnique = true,
+  });
   Future<List<StoredSecret>> getSecrets();
   Future<bool> setSecret(StoredSecret secret);
   Future<bool> importSecrets(List<StoredSecret> secrets);
