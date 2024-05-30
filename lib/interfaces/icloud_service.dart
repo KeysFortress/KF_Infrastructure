7e51b4f0380bacf7c6535bb8eaf3b45e7a73885e
Kristifor Milchev <km@airzen.io>
2024-05-30 14:36:14 +0300

    Adding begin connection.

--- lib/interfaces/icloud_service.dart
+++ lib/interfaces/icloud_service.dart
@@ -1,6 +1,7 @@
 import 'package:domain/models/cloud_connection_code.dart';
 
 abstract class ICloudService {
+  Future<CloudConnectionCode?> begin(String setupUrl);
   Future<String> initConnection(CloudConnectionCode code);
   Future<bool> connect(CloudConnectionCode code, String signature);
   Future<bool> disconnect(CloudConnectionCode code);
