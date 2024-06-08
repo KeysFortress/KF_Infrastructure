ea2e28d9aecb3d6a132b05bceb1a77f7bd7920f5
Kristifor Milchev <km@airzen.io>
2024-06-08 12:07:36 +0300

    Addingthe device name as parameter to the cloud connect service connected method.

--- lib/interfaces/icloud_service.dart
+++ lib/interfaces/icloud_service.dart
@@ -4,7 +4,11 @@ import 'package:domain/models/cloud_connection_code.dart';
 abstract class ICloudService {
   Future<CloudConnectionCode?> begin(String setupUrl);
   Future<String> initConnection(CloudConnectionCode code);
-  Future<bool> connect(CloudConnectionCode code, String signature);
+  Future<bool> connect(
+    CloudConnectionCode code,
+    String signature,
+    String deviceName,
+  );
   Future<bool> disconnect(CloudConnectionCode code);
   Future<List<CloudConnectionCode>> connections();
   Future<SimpleKeyPair> getOrCreateIdentity();
